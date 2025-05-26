d93c04cd3f8bbb6ef823f225364d6f32898fd0ab
.regele <vi9lomister@gmail.com>
2025-05-26 14:45:02 +0100

    Updated Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -123,7 +123,7 @@ const Sidebar = ({ active, onChange, user }) => {
         flexDirection: 'column', 
         alignItems: 'center',
         gap: 4,
-        marginTop: '-400px'
+        marginTop: '0px'
       }}>
         <motion.button
           whileHover={{ scale: 1.1 }}
